d253afc61871aec321357acf33ee52f79c13df2f
dpowar <devendra.powar@rediffmail.com>
2024-03-09 10:12:35 +0530

    studet list add

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { ParentComponent } from './parent/parent.component';
+import { StudentListComponent } from './student-list/student-list.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FooterComponent, ParentComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, ParentComponent, StudentListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
